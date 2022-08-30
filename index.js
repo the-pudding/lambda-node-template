3896b9541962203bf0a53c53a4b9e4789fcdae1f
russellgoldenberg <russellgoldenberg@gmail.com>
2022-08-30 14:21:21 -0400

    datas3

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 // import dataS3 from "data-s3";
 
 const dev = process.env.NODE_ENV == "development";
-
+// const bucket = "pudding.cool";
+// const region = "us-east-1";
+// const path = "path/to/folder";
 
 const init = async () => {
 	// const accessKeyId = process.env.S3_ACCESS_KEY_ID;
 	// const secretAccessKey = process.env.S3_SECRET_ACCESS_KEY;
-	// const bucket = "pudding.cool";
-	// const region = "us-east-1";
-	// const path = "path/to/folder";
 
 	// dataS3.init({ accessKeyId, secretAccessKey, region });
 };
